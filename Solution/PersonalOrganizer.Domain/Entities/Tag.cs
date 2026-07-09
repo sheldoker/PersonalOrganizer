987f05f5b86f0147254dc9c95e3023bf37ec0dcb
Sheldoker <sheldoker@mail.ru>
2026-07-09 20:52:29 +0500

    Add tag entities

---
+++ Solution/PersonalOrganizer.Domain/Entities/Tag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PersonalOrganizer.Domain.Entities
+{
+    public class Tag
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Tag(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Название не должно быть пустым", nameof(name));
+            }
+            Name = name;
+        }
+
+        public void Rename(string name)
+        {
+            if (Name == name)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Название не должно быть пустым", nameof(name));
+            }
+
+            Name = name;
+        }
+    }
+}
