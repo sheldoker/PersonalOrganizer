32313fdda975f0d7b6a72906732d7fca2d2e9274
Sheldoker <sheldoker@mail.ru>
2026-07-14 16:43:12 +0500

    Add CategoryRepository and TagRepisitory

---
+++ Solution/PersonalOrganizer.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using PersonalOrganizer.Domain.Repositories;
+using PersonalOrganizer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using PersonalOrganizer.Domain.Entities;
+
+namespace PersonalOrganizer.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Category category)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Solution/PersonalOrganizer.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,43 @@
+﻿using PersonalOrganizer.Domain.Repositories;
+using PersonalOrganizer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using PersonalOrganizer.Domain.Entities;
+
+namespace PersonalOrganizer.Infrastructure.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly AppDbContext _context;
+        public TagRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Tag tag)
+        {
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Tag>> GetAllAsync()
+        {
+            return await _context.Tags.ToListAsync();
+        }
+
+        public async Task<Tag?> GetByIdAsync(int id)
+        {
+            return await _context.Tags.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Tag tag)
+        {
+            _context.Tags.Update(tag);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
