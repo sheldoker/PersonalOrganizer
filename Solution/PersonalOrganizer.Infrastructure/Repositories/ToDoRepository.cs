d20a081f0ea6d72d813b3a9ed945b0988884444a
Sheldoker <sheldoker@mail.ru>
2026-07-14 16:20:04 +0500

    Add ToDoRepository

---
+++ Solution/PersonalOrganizer.Infrastructure/Repositories/ToDoRepository.cs
@@ -0,0 +1,44 @@
+﻿using PersonalOrganizer.Domain.Repositories;
+using PersonalOrganizer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using PersonalOrganizer.Domain.Entities;
+
+namespace PersonalOrganizer.Infrastructure.Repositories
+{
+    public class ToDoRepository : IToDoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ToDoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(ToDo toDo)
+        {
+            _context.ToDos.Add(toDo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ToDo toDo)
+        {
+            _context.ToDos.Remove(toDo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<ToDo>> GetAllAsync()
+        {
+            return await _context.ToDos.ToListAsync();
+        }
+
+        public async Task<ToDo?> GetByIdAsync(int id)
+        {
+            return await _context.ToDos.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(ToDo toDo)
+        {
+            _context.ToDos.Update(toDo);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
