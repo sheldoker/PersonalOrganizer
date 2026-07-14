0c7686c199e14089735fb34993607a73e6c32343
Sheldoker <sheldoker@mail.ru>
2026-07-14 16:06:25 +0500

    Created Ifrastructere project

---
+++ Solution/PersonalOrganizer.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalOrganizer.Domain.Entities;
+
+namespace PersonalOrganizer.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
---
+++ Solution/PersonalOrganizer.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,46 @@
+﻿using PersonalOrganizer.Domain.Entities;
+using PersonalOrganizer.Domain.Repositories;
+using PersonalOrganizer.Infrastructure.Persistence;
+using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalOrganizer.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public NoteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Note note)
+        {
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Note note)
+        {
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Note>> GetAllAsync()
+        {
+            return await _context.Notes.ToListAsync();
+        }
+
+        public async Task<Note?> GetByIdAsync(int id)
+        {
+            return await _context.Notes.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Note note)
+        {
+            _context.Notes.Update(note);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
